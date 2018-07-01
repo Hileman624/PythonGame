c24fa4415cd55a2ba4d5c6d9c7f1584861566801
dhileman823 <dhileman823@gmail.com>
2018-06-30 23:17:20 -0400

    first push test

--- Shops_an_Stuff.py
+++ Shops_an_Stuff.py
@@ -16,22 +16,23 @@ def springfield_shop(player):
         while is_valid == 0:
             print("We have consumables and materials.")
             shop = input("Which one would you like to browse? [CONSUMABLES/MATERIALS} If you have changed your mind, and would like to leave the shop [NO] ")
-                if shop.upper() == 'CONSUMABLES':
-                    is_valid = 1
-                    shop_consumables = input('MINOR HEALTH POTION'
-                                             'MINOR MANA POTION')
+            if shop.upper() == 'CONSUMABLES':
+                is_valid = 1
+                shop_consumables = input('MINOR HEALTH POTION OR MINOR MANA POTION')
 
-                    if shop_consumables.upper() == 'MINOR HEALTH POTION':
-                        player.inventory.append(minor_health_potion)
+                if shop_consumables.upper() == 'MINOR HEALTH POTION':
+                    player.inventory.append(minor_health_potion)
 
-                if shop.upper() == 'MATERIALS':
-                    is_valid = 1
-                    shop_materials = input('')
-                if shop.upper() == 'NO':
-                    is_valid = 1
+            if shop.upper() == 'MATERIALS':
+                is_valid = 1
+                shop_materials = input('')
+            if shop.upper() == 'NO':
+                is_valid = 1
 
     if would_you_like_to_shop.upper() == 'NO':
         print("Okay, if you change your mind, I'll be here!")
         return
 
-def springfield_smith(player):
+#def springfield_smith(player):
+
+#david was here
