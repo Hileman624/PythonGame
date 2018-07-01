87df2db80b298acf0dbe700da0ce9fa57694b3ec
dhileman823 <dhileman823@gmail.com>
2018-07-01 12:05:06 -0400

    change to main

--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
 from graphics import *
 
 def main():
-    win = GraphWin("My Circle", 100, 100)
-    c = Circle(Point(50,50), 10)
-    c.draw(win)
+    win = GraphWin("Super Python RPG", 800, 600)
+    rect = Rectangle(Point(10,10), Point(100,100))
+    rect.draw(win)
     win.getMouse() # Pause to view result
     win.close()    # Close window when done
 
